ac9fa3a44373da5188db902857d93fde03b18b7d
RachaelMuriithi <rachael.mureithi@student.moringaschool.com>
2022-08-21 21:48:05 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Code your solution here
+const drivers = ["Bobby", "Sammy", "Sally", "Annette", "Sarah", "Bobby"];
+
+function findMatching(drivers, str) {
+  return drivers.filter(function (driver) {
+    return driver.toLowerCase() === str.toLowerCase();
+  });
+}
+// console.log(findMatching(drivers, "Bobby")
+
+function fuzzyMatch(drivers, str) {
+  return drivers.filter(function (getDriver) {
+    return (
+      getDriver.toLowerCase().substring(0, str.length) === str.toLowerCase()
+    );
+  });
+}
+function matchName(drivers, str) {
+    return drivers.filter(function (driverProp) {
+      return driverProp.name === str;
+    });
+  }
